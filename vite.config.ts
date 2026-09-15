c5bf5260d1f46212af55ab68292ea9638dddd186
Kamaria DeRamus <kmoe@Kamarias-MacBook-Pro.local>
2026-09-15 15:02:15 -0400

    adding cloudfare database struction

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), cloudflare()],
 })
